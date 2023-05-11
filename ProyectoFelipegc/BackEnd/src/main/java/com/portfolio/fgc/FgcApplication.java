f9a7240084753276fb5c35b8b9b686da91c9cc7f
FeliGC <104606462+FeliGC@users.noreply.github.com>
2023-05-11 13:45:13 -0300

    Creación Carpeta FrontEnd
    Creación Carpeta FrontEnd

---
+++ ProyectoFelipegc/BackEnd/src/main/java/com/portfolio/fgc/FgcApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.fgc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FgcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FgcApplication.class, args);
+	}
+
+}
---
+++ ProyectoFelipegc/BackEnd/src/test/java/com/portfolio/fgc/FgcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.fgc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FgcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
